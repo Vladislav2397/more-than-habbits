f655ef9895007078a217afb46584257da797f782
Vl@dislav <vt@burno.tech>
2023-05-21 18:38:31 +0300

    Initialize project

---
+++ src/vue.d.ts
@@ -0,0 +1,14 @@
+// https://ru.vuejs.org/v2/guide/typescript.html
+
+// 1. Обязательно импортируйте Vue перед объявлением расширенных типов
+import Vue from 'vue'
+import { type DeviceProviderValue } from './globals'
+
+// 2. Укажите файл с типами, которые вы хотите расширить
+//    Vue имеет тип конструктора в types/vue.d.ts
+declare module 'vue/types/vue' {
+    // 3. Объявите расширение для Vue
+    interface Vue {
+        $device: DeviceProviderValue
+    }
+}
